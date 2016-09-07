7d501e29220924260d3ffc857d6990bed545aac0
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-09-07 09:18:14 -0600

    allowed adding pieces and expanding board

--- game_board/game_board.py
+++ game_board/game_board.py
@@ -4,8 +4,34 @@ class GameBoard:
         self._board = []
 
     def play(self, piece, x=0, y=0):
-
         if len(self._board) == 0:
             self._board = [[None] * 3 for i in range(3)]
             self._board[1][1] = piece
 
+            return
+
+        self._board[y][x] = piece
+        self._pad_board()
+
+    def _pad_board(self):
+        """Ensures there is a padding of empty spots around the board"""
+
+        # Check for top padding
+        if any(self._board[0][i] is not None for i in range(len(self._board[0]))):
+            self._board.insert(0, [None] * len(self._board))
+
+        # Check for bottom padding
+        bottom = len(self._board) - 1
+        if any(self._board[bottom][i] is not None for i in range(len(self._board[0]))):
+            self._board += [[None] * len(self._board)]
+
+        # Left padding
+        if any(self._board[i][0] is not None for i in range(len(self._board))):
+            for i in range(len(self._board)):
+                self._board[i].insert(0, None)
+
+        # Right padding
+        right = len(self._board[0]) - 1
+        if any(self._board[i][right] is not None for i in range(len(self._board))):
+            for i in range(len(self._board)):
+                self._board[i] += [None]
--- tests/game_board_test.py
+++ tests/game_board_test.py
@@ -12,3 +12,48 @@ class GameBoardTest(TestCase):
         self.assertEqual(3, len(board._board))
         self.assertEqual(3, len(board._board[0]))
         self.assertEqual(piece, board._board[1][1])
+
+    def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_right(self):
+        board = GameBoard()
+        piece = Piece()
+        board.play(piece)
+
+        board.play(piece, x=2, y=1)
+
+        self.assertEqual(3, len(board._board))
+        self.assertEqual(4, len(board._board[0]))
+        self.assertEqual(piece, board._board[1][2])
+
+    def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_left(self):
+        board = GameBoard()
+        piece = Piece()
+        board.play(piece)
+
+        board.play(piece, x=0, y=1)
+
+        self.assertEqual(3, len(board._board))
+        self.assertEqual(4, len(board._board[0]))
+        self.assertEqual(piece, board._board[1][1])
+
+    def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_up(self):
+        board = GameBoard()
+        piece = Piece()
+        board.play(piece)
+
+        board.play(piece, x=1, y=0)
+
+        self.assertEqual(4, len(board._board))
+        self.assertEqual(3, len(board._board[0]))
+        self.assertEqual(piece, board._board[1][1])
+
+    def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_down(self):
+        board = GameBoard()
+        piece = Piece()
+        board.play(piece)
+
+        board.play(piece, x=1, y=2)
+
+        self.assertEqual(4, len(board._board))
+        self.assertEqual(3, len(board._board[0]))
+        self.assertEqual(piece, board._board[2][1])
+        
