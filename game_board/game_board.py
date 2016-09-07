e1b4eff8d9c64e94b7f7d08bab2b873d13769f99
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-09-07 11:17:08 -0600

    validation

--- game_board/game_board.py
+++ game_board/game_board.py
@@ -6,40 +6,116 @@ class GameBoard:
 
     def __init__(self):
         self._board = []
+        self._saved_board = []
+        self._plays = []
+
+    def reset_board(self):
+        """Clear the current board"""
+        pass
+
+    def start_turn(self):
+        """Start a turn"""
+        self._board = self._saved_board.copy()
+        self._plays = []
 
     def play(self, piece, x=0, y=0):
+        """Play a tile"""
         if len(self._board) == 0:
             self._board = [[None] * 3 for i in range(3)]
             self._board[1][1] = piece
             return
 
-        if self._is_invalid_play(piece, x, y):
+        if not self._is_play_valid(piece, x, y):
             raise InvalidPlayException()
 
         self._board[y][x] = piece
         self._pad_board()
 
-    def _is_invalid_play(self, piece, x, y):
-        """Validates a move is within the board, not on the corners, and valid in its row/column"""
+    def score(self):
+        """Return the score for the current turn"""
+        pass
+
+    def end_turn(self):
+        """End the current turn"""
+        self._saved_board = self._board.copy()
+
+    def reset_turn(self):
+        """Reset the board to the way it was at the beginning of the turn"""
+        self._board = self._saved_board.copy()
+
+    def _is_play_valid(self, piece, x, y):
+        """Validates a move is within the board, not on the corners, not replacing a existing piece, adjacent to an
+           existing tile and valid in its row/column"""
         if x < 0 or x >= len(self._board[0]):
-            return True
-
+            return False
         if y < 0 or y >= len(self._board):
-            return True
-
+            return False
         if x == 0 and y == 0:
-            return True
-
+            return False
         if x == 0 and y == len(self._board) - 1:
-            return True
-
+            return False
         if x == len(self._board[0]) - 1 and y == len(self._board) - 1:
-            return True
-
+            return False
         if x == len(self._board[0]) - 1 and y == 0:
+            return False
+
+        if self._board[y][x] is not None:
+            return False
+
+        row = [piece]
+        t_x = x + 1
+        while t_x < len(self._board[0]) and self._board[y][t_x] is not None:
+            row.append(self._board[y][t_x])
+            t_x += 1
+
+        t_x = x - 1
+        while t_x >= 0 and self._board[y][t_x] is not None:
+            row.append(self._board[y][t_x])
+            t_x -= 1
+
+        if not self._is_row_valid(row):
+            return False
+
+        row = [piece]
+        t_y = y + 1
+        while t_y < len(self._board) and self._board[t_y][x] is not None:
+            row.append(self._board[t_y][x])
+            t_y += 1
+
+        t_y = y - 1
+        while t_y >= 0 and self._board[t_y][x] is not None:
+            row.append(self._board[t_y][x])
+            t_y -= 1
+
+        if not self._is_row_valid(row):
+            return False
+
+        return True
+
+    def _is_row_valid(self, row):
+        """If all row colors are equal, check each shape shows up at most once. If all shapes are equal, check each
+           color shows up at most once. Otherwise the row is invalid."""
+        if len(row) == 1:
             return True
 
-        return False
+        if all(row[i].color == row[0].color for i in range(len(row))):
+            shapes = []
+            for i in range(len(row)):
+                if row[i].shape in shapes:
+                    return False
+                shapes.append(row[i].shape)
+
+        elif all(row[i].shape == row[0].shape for i in range(len(row))):
+            colors = []
+            for i in range(len(row)):
+                if row[i].color in colors:
+                    return False
+                colors.append(row[i].color)
+
+        else:
+            return False
+
+        return True
 
     def _pad_board(self):
         """Ensures there is a padding of empty spots around the board"""
--- tests/game_board_test.py
+++ tests/game_board_test.py
@@ -15,76 +15,92 @@ class GameBoardTest(TestCase):
 
     def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_right(self):
         board = GameBoard()
-        piece = Piece()
-        board.play(piece)
+        piece1 = Piece(color=COLORS.RED, shape=SHAPES.SPARKLE)
+        piece2 = Piece(color=COLORS.GREEN, shape=SHAPES.SPARKLE)
+        board.play(piece1)
 
-        board.play(piece, x=2, y=1)
+        board.play(piece2, x=2, y=1)
 
         self.assertEqual(3, len(board._board))
         self.assertEqual(4, len(board._board[0]))
-        self.assertEqual(piece, board._board[1][2])
+        self.assertEqual(piece2, board._board[1][2])
 
     def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_left(self):
         board = GameBoard()
-        piece = Piece()
-        board.play(piece)
+        piece1 = Piece(color=COLORS.RED, shape=SHAPES.SPARKLE)
+        piece2 = Piece(color=COLORS.GREEN, shape=SHAPES.SPARKLE)
+        board.play(piece1)
 
-        board.play(piece, x=0, y=1)
+        board.play(piece2, x=0, y=1)
 
         self.assertEqual(3, len(board._board))
         self.assertEqual(4, len(board._board[0]))
-        self.assertEqual(piece, board._board[1][1])
+        self.assertEqual(piece2, board._board[1][1])
 
     def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_up(self):
         board = GameBoard()
-        piece = Piece()
-        board.play(piece)
+        piece1 = Piece(color=COLORS.RED, shape=SHAPES.SPARKLE)
+        piece2 = Piece(color=COLORS.GREEN, shape=SHAPES.SPARKLE)
+        board.play(piece1)
 
-        board.play(piece, x=1, y=0)
+        board.play(piece2, x=1, y=0)
 
         self.assertEqual(4, len(board._board))
         self.assertEqual(3, len(board._board[0]))
-        self.assertEqual(piece, board._board[1][1])
+        self.assertEqual(piece2, board._board[1][1])
 
     def test_playing_piece_adds_piece_to_board_at_correct_position_and_expands_board_down(self):
         board = GameBoard()
-        piece = Piece()
-        board.play(piece)
+        piece1 = Piece(color=COLORS.RED, shape=SHAPES.SPARKLE)
+        piece2 = Piece(color=COLORS.GREEN, shape=SHAPES.SPARKLE)
+        board.play(piece1)
 
-        board.play(piece, x=1, y=2)
+        board.play(piece2, x=1, y=2)
 
         self.assertEqual(4, len(board._board))
         self.assertEqual(3, len(board._board[0]))
-        self.assertEqual(piece, board._board[2][1])
+        self.assertEqual(piece2, board._board[2][1])
 
     def test_playing_pieces_and_expanding_board(self):
         board = GameBoard()
-        piece = Piece(color=COLORS.RED, shape=SHAPES.SPARKLE)
-        board.play(piece)
-        board.play(piece, x=1, y=2)
-        board.play(piece, x=1, y=3)
-        board.play(piece, x=1, y=4)
-        board.play(piece, x=2, y=2)
-        board.play(piece, x=3, y=2)
-        board.play(piece, x=4, y=2)
-        board.play(piece, x=0, y=2)
-        board.play(piece, x=0, y=2)
-        board.play(piece, x=0, y=2)
-        board.play(piece, x=1, y=1)
-        board.play(piece, x=1, y=0)
-        board.play(piece, x=1, y=0)
-        board.play(piece, x=1, y=0)
+        piece1 = Piece(color=COLORS.RED, shape=SHAPES.SPARKLE)
+        piece2 = Piece(color=COLORS.RED, shape=SHAPES.SQUARE)
+        piece3 = Piece(color=COLORS.RED, shape=SHAPES.CIRCLE)
+        piece5 = Piece(color=COLORS.GREEN, shape=SHAPES.SQUARE)
+        piece6 = Piece(color=COLORS.BLUE, shape=SHAPES.SQUARE)
+        piece7 = Piece(color=COLORS.CYAN, shape=SHAPES.SQUARE)
+        piece8 = Piece(color=COLORS.YELLOW, shape=SHAPES.SQUARE)
+        piece9 = Piece(color=COLORS.MAGENTA, shape=SHAPES.SQUARE)
+        piece10 = Piece(color=COLORS.MAGENTA, shape=SHAPES.SPARKLE)
+        piece11 = Piece(color=COLORS.MAGENTA, shape=SHAPES.CIRCLE)
+        piece12 = Piece(color=COLORS.MAGENTA, shape=SHAPES.STAR)
+        piece13 = Piece(color=COLORS.MAGENTA, shape=SHAPES.TRIANGLE)
+        piece14 = Piece(color=COLORS.MAGENTA, shape=SHAPES.DIAMOND)
+
+        board.play(piece1)
+        board.play(piece2, x=1, y=2)
+        board.play(piece3, x=1, y=3)
+        board.play(piece5, x=2, y=2)
+        board.play(piece6, x=3, y=2)
+        board.play(piece7, x=4, y=2)
+        board.play(piece8, x=0, y=2)
+        board.play(piece9, x=0, y=2)
+        board.play(piece10, x=1, y=1)
+        board.play(piece11, x=1, y=0)
+        board.play(piece12, x=1, y=0)
+        board.play(piece13, x=1, y=0)
+        board.play(piece14, x=1, y=0)
 
         expected_board = [
-            [None, None, None, None, None, None, None, None, None, ],
-            [None, piece, None, None, None, None, None, None, None, ],
-            [None, piece, None, None, None, None, None, None, None, ],
-            [None, piece, None, None, None, None, None, None, None, ],
-            [None, piece, None, None, piece, None, None, None, None, ],
-            [None, piece, piece, piece, piece, piece, piece, piece, None, ],
-            [None, None, None, None, piece, None, None, None, None, ],
-            [None, None, None, None, piece, None, None, None, None, ],
-            [None, None, None, None, None, None, None, None, None, ],
+            [None, None, None, None, None, None, None, None, ],
+            [None, piece14, None, None, None, None, None, None ],
+            [None, piece13, None, None, None, None, None, None],
+            [None, piece12, None, None, None, None, None, None ],
+            [None, piece11, None, None, None, None, None, None ],
+            [None, piece10, None, piece1, None, None, None, None, ],
+            [None, piece9, piece8, piece2, piece5, piece6, piece7, None, ],
+            [None, None, None,  piece3, None, None, None, None, ],
+            [None, None, None, None, None, None, None, None, ],
         ]
 
         self.assertEqual(expected_board, board._board)
@@ -96,3 +112,29 @@ class GameBoardTest(TestCase):
 
         with self.assertRaises(InvalidPlayException):
             board.play(piece, x=0, y=0)
+
+    def test_raises_placement_error_when_placing_invalid_tiles_adjacent_horizontally(self):
+        board = GameBoard()
+        red_circle = Piece(shape=SHAPES.CIRCLE, color=COLORS.RED)
+        board.play(red_circle)
+
+        green_circle = Piece(shape=SHAPES.CIRCLE, color=COLORS.GREEN)
+        board.play(green_circle, x=0, y=1)
+
+        green_triangle = Piece(shape=SHAPES.TRIANGLE, color=COLORS.GREEN)
+
+        with self.assertRaises(InvalidPlayException):
+            board.play(green_triangle, x=0, y=1)
+
+    def test_raises_placement_error_when_placing_invalid_tiles_adjacent_vertically(self):
+        board = GameBoard()
+        red_circle = Piece(shape=SHAPES.CIRCLE, color=COLORS.RED)
+        board.play(red_circle)
+
+        green_circle = Piece(shape=SHAPES.CIRCLE, color=COLORS.GREEN)
+        board.play(green_circle, x=1, y=2)
+
+        green_triangle = Piece(shape=SHAPES.TRIANGLE, color=COLORS.GREEN)
+
+        with self.assertRaises(InvalidPlayException):
+            board.play(green_triangle, x=1, y=3)
