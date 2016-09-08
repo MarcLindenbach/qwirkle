e48d1fda2a9ddec1b6bdb43aeea56dfe6b547422
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-09-08 12:58:55 -0600

    cleaned up game

--- qwirkle.py
+++ qwirkle.py
@@ -14,8 +14,6 @@ class QwirkleGame:
         self._board = GameBoard()
         self._generate_new_bag_of_tiles()
 
-        print('Qwirkle Time!')
-
         player_number = 1
         for player in players:
             if player == 'greedy_bot':
@@ -28,9 +26,20 @@ class QwirkleGame:
                 raise ValueError('%s is an invalid player type' % player)
             player_number += 1
 
+        score_message = (-1, 0)
         current_player = 0
         while True:
-            print('%s Turn!' % self._players[current_player].name())
+            print('\n' * 50)
+            print('Qwirkle Hard\n')
+
+            print('  Score:')
+            for i in range(len(self._players)):
+                message = '    %s - %i' % (self._players[i].name(), self._players[i].score())
+                if score_message[0] == i:
+                    message += ' +%i' % score_message[1]
+                print(message)
+            print('\n  It is %ss turn\n' % self._players[current_player].name())
+
             self._board.print_board()
             self._players[current_player].pick_tiles(self._bag_of_tiles)
             self._board.start_turn()
@@ -39,18 +48,15 @@ class QwirkleGame:
             score = self._board.score()
             self._players[current_player].add_points(score)
 
-            print('%s got %i points!' % (self._players[current_player].name(), score))
+            score_message = (current_player, score)
             self._board.end_turn()
 
             if score == 0:
-                print('%s is exchanging tiles...' % self._players[current_player].name())
+                print('  %s is exchanging tiles...' % self._players[current_player].name())
                 self._bag_of_tiles += self._players[current_player].get_tiles()
                 self._players[current_player].clear_tiles()
 
             self._players[current_player].pick_tiles(self._bag_of_tiles)
-
-            for i in range(len(self._players)):
-                print('%s - %i' % (self._players[i].name(), self._players[i].score()))
 
             if self._players[current_player].has_no_tiles():
                 break
@@ -61,7 +67,12 @@ class QwirkleGame:
 
         winning_player = max(self._players, key=lambda p: p.score())
 
-        print('%s wins!' % winning_player.name())
+        print('\n  Final Score:')
+        for i in range(len(self._players)):
+            message = '    %s - %i' % (self._players[i].name(), self._players[i].score())
+            print(message)
+
+        print('\n  %s wins!\n' % winning_player.name())
 
     def _generate_new_bag_of_tiles(self):
         self._bag_of_tiles = []
