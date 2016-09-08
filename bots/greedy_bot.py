e085ec676ca5fe6ece0f3a01ed171d4d034fd18b
Marc Lindenbach <marc.lindenbach@gmail.com>
2016-09-08 12:23:58 -0600

    plays multiple tiles at once

--- bots/greedy_bot.py
+++ bots/greedy_bot.py
@@ -1,3 +1,4 @@
+import copy
 from player import Player
 from game_board import InvalidPlayException
 
@@ -11,16 +12,32 @@ class GreedyBot(Player):
             tiles = self._tiles.copy()
 
             for i in range(len(tiles)):
+                tile_played = False
                 try:
                     board.play(tiles[i], x=x, y=y)
                     plays.append({
                         'plays': [(x, y, tiles[i])],
                         'score': board.score()
                     })
-                    tiles.pop(i)
-                    break
+                    tiles_remaining = tiles.copy()
+                    tiles_remaining.pop(i)
+                    tile_played = True
                 except InvalidPlayException:
                     pass
+
+                while tile_played:
+                    tile_played = False
+                    for (nx, ny) in board.valid_plays():
+                        for j in range(len(tiles_remaining)):
+                            try:
+                                board.play(tiles_remaining[j], x=nx, y=ny)
+                                plays[-1]['plays'].append((nx, ny, tiles_remaining[j]))
+                                plays[-1]['score'] = board.score()
+                                tiles_remaining.pop(j)
+                                tile_played = True
+                                break
+                            except InvalidPlayException:
+                                pass
 
             board.reset_turn()
 
